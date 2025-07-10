af634d7e59e299c0d6f2655a3ad72328a4b051ab
Agrim Jaimini <aj638@cornell.edu>
2025-07-10 01:30:37 -0500

    fixed backend

--- backend/main.py
+++ backend/main.py
@@ -67,3 +67,7 @@ async def occupancy_hook(payload: RadarPayload):
 @app.get("/")
 async def home():
     return "hello"
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
