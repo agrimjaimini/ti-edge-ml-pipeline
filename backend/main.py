3c1dcf51a30a8eaa1fea35c2b29d75624cc42d4d
Agrim Jaimini <aj638@cornell.edu>
2025-07-10 01:31:22 -0500

    fixed port

--- backend/main.py
+++ backend/main.py
@@ -70,4 +70,4 @@ async def home():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
